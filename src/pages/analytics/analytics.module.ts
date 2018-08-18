59a661ab49afcac2a1564f24b5c379bce0bee928
Estácio Pereira <estacio.pereira@gmail.com>
2018-08-18 13:32:25 -0300

    Add some features to analytics page

--- src/pages/analytics/analytics.module.ts
+++ src/pages/analytics/analytics.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChartsModule } from 'ng2-charts';
 
+import { ComponentsModule } from './../../components/components.module';
 import { AnalyticsPage } from './analytics';
 
 @NgModule({
   declarations: [AnalyticsPage],
-  imports: [IonicPageModule.forChild(AnalyticsPage), ChartsModule],
+  imports: [
+    IonicPageModule.forChild(AnalyticsPage),
+    ChartsModule,
+    ComponentsModule,
+  ],
 })
 export class AnalyticsPageModule {}
--- src/pages/analytics/analytics.ts
+++ src/pages/analytics/analytics.ts
@@ -13,4 +13,28 @@ import { IonicPage } from 'ionic-angular';
   selector: 'page-analytics',
   templateUrl: 'analytics.html',
 })
-export class AnalyticsPage {}
+export class AnalyticsPage {
+  matches: any[];
+
+  constructor() {
+    this.matches = [];
+    for (let i = 0; i < 5; i++)
+      this.matches.push({
+        siglaTipo: 'PL',
+        ementa:
+          'Institui a obrigatoriedade de estabelecimentos públicos e privados voltados ao ensino ou recreação infantil e fundamental a capacitarem seu corpo docente e funcional em noções básicas de primeiros socorros.',
+        statusProposicao: {
+          dataHora: '2018-03-22T00:00',
+          regime: 'Urgência (Art. 155, RICD)',
+          descricaoTramitacao: 'Devolução à CCP',
+          descricaoSituacao: 'Aguardando Apreciação pelo Senado Federal',
+          despacho: 'Devolução à CCP.',
+          orgao: {
+            sigla: 'CCJC',
+            nome: 'Comissão de Constituição e Justiça e de Cidadania',
+            tipoOrgao: 'Comissão Permanente',
+          },
+        },
+      });
+  }
+}
