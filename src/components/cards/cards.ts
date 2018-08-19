d2a8df3104a1ca54e1cc06bb0f5671827b34bd9b
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2018-08-19 02:30:54 -0300

    CONSERTEI

--- src/components/cards/cards.ts
+++ src/components/cards/cards.ts
@@ -5,17 +5,19 @@ import { DragEvent, SwingStackComponent, SwingCardComponent } from 'angular2-swi
     selector: 'cards',
     templateUrl: 'cards.html'
 })
-export class CardsComponent implements OnInit{
+export class CardsComponent implements OnInit {
     @ViewChild('myswing1') swingStack: SwingStackComponent;
     @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
     @ViewChildren('aprovo') aprovo: QueryList<ElementRef>;
     @ViewChildren('reprovo') reprovo: QueryList<ElementRef>;
 
-    @Input() 
+    @Input()
     public cards: Array<any>;
 
-    @Input() 
+    @Input()
     public onSwipe: Function;
+
+    private locked: Date = new Date();
 
     ngOnInit() {
     }
@@ -31,17 +33,23 @@ export class CardsComponent implements OnInit{
             return 800;
         }
     };
-    
+
     ngAfterViewInit() {
         this.setOpAp('0');
         this.setOpRe('0');
         // Either subscribe in controller or set in HTML
         this.swingStack.throwin.subscribe((event: DragEvent) => {
-            // console.log(event)
             event.target.style.background = '#ffffff !important';
+            console.log('primeiro callback');
             this.setOpAp('0');
             this.setOpRe('0');
         });
+        this.swingStack.throwout.subscribe(() => {
+            console.log('segundo callback');
+            this.aprovo.forEach((e) => this.setOpAp('0', e));
+            this.reprovo.forEach((e) => this.setOpRe('0', e));
+            this.locked = new Date();
+        })
     }
 
     onItemMove(element, x, y, r) {
@@ -50,22 +58,25 @@ export class CardsComponent implements OnInit{
         let min = Math.trunc(Math.min(16 * 16 - abs, 16 * 16));
         let hexCode = this.decimalToHex(min, 2);
 
-        if (x < 0) {
-            color = '#FF' + hexCode + hexCode;            
-            this.setOpAp('0');;
-            this.setOpRe(Math.min(1, Math.abs(x) / 300));
-        } else {
-            this.setOpRe('0');
-            this.setOpAp(Math.min(1, x / 300));
-            color = '#' + hexCode + 'FF' + hexCode;
+        const waitTime = 2 * 1000;
+        if (new Date().getTime() - this.locked.getTime() > waitTime) {
+            if (x < 0) {
+                color = '#FF' + hexCode + hexCode;
+                this.setOpAp('0');;
+                this.setOpRe(Math.min(1, Math.abs(x) / 300));
+            } else {
+                this.setOpRe('0');
+                this.setOpAp(Math.min(1, x / 300));
+                color = '#' + hexCode + 'FF' + hexCode;
+            }
         }
-        
+
         element.style.background = color;
         element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
     }
 
     onThrow(keep: boolean) {
-        const removedCard = this.cards.pop();      
+        const removedCard = this.cards.pop();
 
         this.onSwipe(keep, removedCard);
     }
@@ -80,13 +91,13 @@ export class CardsComponent implements OnInit{
 
         return hex;
     }
-    setOpAp(o) {
-        if (this.aprovo.last)
-            this.aprovo.last.nativeElement.style.opacity = o;
+    setOpAp(o, e = null) {
+        if (this.aprovo.last || e)
+            (e || this.aprovo.last).nativeElement.style.opacity = o;
     }
-    
-    setOpRe(o) {
-        if (this.reprovo.last)
-        this.reprovo.last.nativeElement.style.opacity = o;
+
+    setOpRe(o, e = null) {
+        if (this.reprovo.last || e)
+            (e || this.reprovo.last).nativeElement.style.opacity = o;
     }
 }
